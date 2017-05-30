b292da019c19723cc1e3ab8d480a720b66185104
Moon Antonio <pinchodev91@gmail.com>
2017-05-30 19:04:40 +0200

    Implementada funcionalidad carga simpel

--- ESSystem Project/Assets/ESSystem Proyecto/Scripts/SimpleCarga.cs
+++ ESSystem Project/Assets/ESSystem Proyecto/Scripts/SimpleCarga.cs
@@ -1,10 +1,10 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// SimpleCarga.cs (00/00/0000)													\\
+// SimpleCarga.cs (30/05/2017)													\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Ejemplo Carga simple										\\
+// Fecha Mod:		30/05/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
@@ -13,8 +13,36 @@ using UnityEngine;
 
 namespace MoonAntonio
 {
+	/// <summary>
+	/// <para>Ejemplo Carga simple</para>
+	/// </summary>
 	public class SimpleCarga : MonoBehaviour 
 	{
+		#region Variables
+		/// <summary>
+		/// <para>Manager de la escena</para>
+		/// </summary>
+		public GameObject manager;                                          // Manager de la escena
+		#endregion
 
+		#region Metodos
+		/// <summary>
+		/// <para>Carga los datos</para>
+		/// </summary>
+		public void Cargar()// Carga los datos
+		{
+			// Accedemos al manager
+			ManagerTest script = manager.GetComponent<ManagerTest>();
+
+			// Cargamos el valor de vida con el nombre vida
+			script.vida = ES2.Load<int>("vida");
+
+			// Guardamos el valor de mana con el nombre mana
+			script.mana = ES2.Load<int>("mana");
+
+			// Guardamos el valor de isAtaque con el nombre atack
+			script.isAtaque = ES2.Load<bool>("atack");
+		}
+		#endregion
 	}
 }
