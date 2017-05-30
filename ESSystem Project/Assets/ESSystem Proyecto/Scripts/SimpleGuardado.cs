4800012f701792e272e0d761dffd6d775e205de1
Moon Antonio <pinchodev91@gmail.com>
2017-05-30 18:56:22 +0200

    Ejemplo guardado

--- ESSystem Project/Assets/ESSystem Proyecto/Scripts/SimpleGuardado.cs
+++ ESSystem Project/Assets/ESSystem Proyecto/Scripts/SimpleGuardado.cs
@@ -1,10 +1,10 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// SimpleGuardado.cs (00/00/0000)													\\
+// SimpleGuardado.cs (30/05/2017)												\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// Descripcion:		Ejemplo Guardado simple										\\
+// Fecha Mod:		30/05/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
@@ -13,8 +13,36 @@ using UnityEngine;
 
 namespace MoonAntonio
 {
+	/// <summary>
+	/// <para>Ejemplo Guardado simple</para>
+	/// </summary>
 	public class SimpleGuardado : MonoBehaviour 
 	{
+		#region Variables
+		/// <summary>
+		/// <para>Manager de la escena</para>
+		/// </summary>
+		public GameObject manager;											// Manager de la escena
+		#endregion
 
+		#region Metodos
+		/// <summary>
+		/// <para>Guarda los datos</para>
+		/// </summary>
+		public void Guardar()// Guarda los datos
+		{
+			// Accedemos al manager
+			ManagerTest script = manager.GetComponent<ManagerTest>();
+
+			// Guardamos el valor de vida con el nombre vida
+			ES2.Save(script.vida, "vida");
+
+			// Guardamos el valor de mana con el nombre mana
+			ES2.Save(script.mana, "mana");
+
+			// Guardamos el valor de isAtaque con el nombre atack
+			ES2.Save(script.isAtaque, "atack");
+		}
+		#endregion
 	}
 }
